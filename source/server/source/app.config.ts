8d2c67e439c337872f7a1430ede212264ecdb200
vitor3g <codevitor3g@gmail.com>
2025-04-19 06:41:56 -0300

    server

---
+++ source/server/source/app.config.ts
@@ -0,0 +1,24 @@
+import { monitor } from "@colyseus/monitor";
+import { playground } from "@colyseus/playground";
+import config from "@colyseus/tools";
+
+export default config({
+  initializeGameServer: () => {
+  },
+
+  initializeExpress: (app) => {
+    app.get("/hello_world", (req, res) => {
+      res.send("It's time to kick ass and chew bubblegum!");
+    });
+
+    if (process.env.NODE_ENV !== "production") {
+      app.use("/", playground());
+    }
+
+    app.use("/monitor", monitor());
+  },
+
+
+  beforeListen: () => {
+  }
+});
---
+++ source/server/source/index.ts
@@ -0,0 +1,5 @@
+import { listen } from "@colyseus/tools";
+
+import app from "./app.config";
+
+listen(app);
